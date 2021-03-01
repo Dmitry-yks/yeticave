113f7c1f1116de50982ad8360f773e63c5f12757
Dmitry-yks <korloyut598@gmail.com>
2021-03-01 13:22:25 +0300

    secondtask

--- index.php
+++ index.php
@@ -27,6 +27,24 @@ $user_name = 'Dimas'; // укажите здесь ваше имя
         <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
 
         <nav class="user-menu">
+            <?php if ($is_auth==1): ?>
+            <div class="user-menu_image">
+                <img src="img/user.jpg" width="40" height="40" alt="Пользователь">
+            </div>
+            <div class="user-menu__logged">
+                <p> <?php echo($user_name); ?> </p>
+            </div>
+
+                <?php else: ?>
+            <ul class="user-menu__list">
+                <li class="user-menu__item">
+                    <a href="#"> Регистрация </a>
+                </li>
+                <li class="user-menu__item">
+                    <a href="#"> Вход </a>
+                </li>
+            </ul>
+            <?php endif;?>
 
         <!-- здесь должен быть PHP код для показа меню и данных пользователя -->
 
